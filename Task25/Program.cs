19d5be3cc12a5f9735e585ceb0847eb3d2048e26
Ekaterina Cheremnykh <eafilippova@gmail.com>
2022-12-11 19:01:12 +0300

    task 25, 27, 29

---
+++ Task25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.Write("Введите число a = ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число b = ");
+int b = int.Parse(Console.ReadLine()!);
+
+int Exponentiation(int number)
+{
+int result = a;
+for(int num = 1; num < b; num++)
+{
+    result = result * a;
+}
+return result;
+}
+Console.WriteLine($"Число {a}, возведенное в степень {b} = {Exponentiation(a)}");
---
+++ Task27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//            452 -> 11
+//            82 -> 10
+//            9012 -> 12
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int SumOfNumber (int number)
+{
+    int sum = 0;
+
+    for (int i = 0; i < number; i++)
+    {
+    sum = sum + number % 10;
+    number = number / 10;
+    }
+
+    return sum;
+}
+
+Console.WriteLine($"Сумма цифр в числе {N} равна {SumOfNumber(N)}");
