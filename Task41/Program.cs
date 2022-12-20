eebca793fddec010b59bcc8a6b8fd58df4e77fec
Ekaterina Cheremnykh <eafilippova@gmail.com>
2022-12-20 12:05:18 +0300

    task 41, 43

---
+++ Task41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] CreateArray(uint size, int minValue, int maxValue)
+{
+    int[] array = new int [size];
+    for(int i = 0; i < size; i++)
+        array[i] = new Random() .Next(minValue, maxValue + 1);
+        return array;
+}
+uint CountOfPositivNumber(int[] array)
+{
+    uint count = 0;
+    foreach(int element in array)
+    if(element > 0)
+    count++;
+    return count;
+}
+int[] array = CreateArray(7, -100, 100);
+Console.WriteLine(String.Join("," , array));
+Console.WriteLine($"В введенных пользователем числaх количество чисел больше 0  = {CountOfPositivNumber(array)}");
---
+++ Task43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Задайте значение b1 для прямой А1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Задайте значение k1 для прямой А1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Задайте значение b2 для прямой А2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Задайте значение k2 для прямой А2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+
+void CanBeCrossLine(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1)/(k1-k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Прямые пересекаются в точке с координатами: ({x}; {y})");
+}
+CanBeCrossLine(b1, k1, b2, k2);
